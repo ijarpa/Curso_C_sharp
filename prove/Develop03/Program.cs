c39d65d6a9cdf09c2056ca24e5ff5665c4d0bb60
Ivan Jarpa <ivanandres.jarpa@gmail.com>
2023-02-09 22:28:47 -0300

    Work in progress

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // call classes
+        Scripture scripture = new Scripture();
+        HideWords hideListClass = new HideWords();
         
+        // lists
+        List<string> scriptureWords = scripture.GetScriptureWords();
+        List<string> hiddenWords = hideListClass.GetHiddenScripture();
+
+
+        Console.WriteLine(string.Join(" ", scriptureWords));
+
+        List<int> selectedIndices = new List<int>();
+        Random rnd = new Random();
+
+        while (selectedIndices.Count < scriptureWords.Count)
+        {
+            Console.WriteLine("Type 'hide' to hide 5 new words, or 'exit' to exit.\n");
+            string input = Console.ReadLine();
+
+            if (input == "exit")
+            {
+                break;
+            }
+            else if (input == "hide")
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    int index = rnd.Next(0, scriptureWords.Count);
+                    while (selectedIndices.Contains(index))
+                    {
+                        index = rnd.Next(0, scriptureWords.Count);
+                    }
+                    selectedIndices.Add(index);
+                    scriptureWords[index] = hiddenWords[index];
+                }
+                Console.Clear();
+                Console.WriteLine(string.Join(" ", scriptureWords));
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
+
     }
 }
