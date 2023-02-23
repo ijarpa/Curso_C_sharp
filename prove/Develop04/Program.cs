95a469bc20b79e01349cc4be126eda8d267d1dd2
Ivan Jarpa <ivanandres.jarpa@gmail.com>
2023-02-23 20:31:22 -0300

    Update

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        int Mindfulness = 0;
+        while (Mindfulness != 4)
+        {
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflecting Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu: ");
+        
+            string userChoice = Console.ReadLine();
+
+            if (userChoice = "1"):
+        }
+
     }
 }
